7d2aa32ce0417b4674f4f91bb937411de7fb1a47
Emilio Cruz <eecruz@quinnpiac.edu>
2023-02-22 13:25:08 -0500

    chapter 3 demo

---
+++ app/src/main/java/edu/quinnipiac/ser210/layouts/LinearLayout.kt
@@ -0,0 +1,11 @@
+package edu.quinnipiac.ser210.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+}
---
+++ app/src/main/java/edu/quinnipiac/ser210/layouts/ScrollView.kt
@@ -0,0 +1,11 @@
+package edu.quinnipiac.ser210.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrollView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scroll_view)
+    }
+}
